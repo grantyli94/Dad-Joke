f1a0078dd8dfa61e463aebc542995ae07779e961
grantyli94 <85194842+grantyli94@users.noreply.github.com>
2021-06-30 11:08:14 -0400

    Update ajaxJoke.js

--- ajaxJoke.js
+++ ajaxJoke.js
@@ -1,67 +1,79 @@
-/*
- * add upvote and downvote button
- *    add event listener to each button reorganizing page
- * add counter associated with each joke
- * 
+/**
+ * Randomly fetch 10 dad jokes from icanhazdadjoke.com and display as an ordered list
+ * Jokes can be up-voted and down-voted, with a running score tally displayed
+ * List is continously reordered with updated score counts
  *  
  */
 
+/* score tracker for each joke */
 const jokeList = [0,0,0,0,0,0,0,0,0,0];
 
-fetchJokes();
+fetchAndDisplayJokes();
 
+/* Fetches 10 jokes and appends them to the joke container */
+async function fetchAndDisplayJokes() {
+  let $container = $(".container");
 
-async function fetchJokes() {
-    let $container = $(".container");
+  for (let i = 0; i < 10; i++) {
+    let joke = await axios.get("https://icanhazdadjoke.com/", { headers: { "Accept": "text/plain" } });
+    let $jokeLine = $(`<li id=${i}>${joke.data}</li>`)
+    let $scoreCount = $(`<span> Score Count: 0 </span>`)
 
-    for (let i = 0; i < 10; i++) {
-        let joke = await axios.get("https://icanhazdadjoke.com/", { headers: { "Accept": "text/plain" } });
-        let $upVote = $(`<button>vote-up</button>`)
-        let $downVote = $(`<button>vote-down</button>`)
-        let $jokeLine = $(`<li id=${i}>${joke.data}</li>`)
-        let $scoreCount = $(`<span> Score Count: 0 </span>`)
+    $jokeLine.append(createUpVoteButton());
+    $jokeLine.append(createDownVoteButton());
+    $jokeLine.append($scoreCount);
 
-        $upVote.on("click", increaseVote);
-        $downVote.on("click", decreaseVote);
-              
-        $jokeLine.append($upVote);
-        $jokeLine.append($downVote);
-        $jokeLine.append($scoreCount);
-        
-        $container.append($jokeLine);
-    }
+    $container.append($jokeLine);
+  }
 }
 
+/* Creates an up-vote button */
+function createUpVoteButton() {
+  let $upVote = $(`<button>vote-up</button>`)
+  $upVote.on("click", increaseVote);
+  return $upVote;
+}
+
+/* Creates a down-vote button */
+function createDownVoteButton() {
+  let $downVote = $(`<button>vote-down</button>`)
+  $downVote.on("click", decreaseVote);
+  return $downVote;
+}
+
+/* Increases vote count and updates Score Count display */
 function increaseVote(evt) {
   evt.preventDefault();
   let i = evt.target.parentElement.id
   jokeList[i] += 1;
   let $scoreCount = $(evt.target.parentElement).find("span")
   $scoreCount.text(`Score Count: ${jokeList[i]}`)
-  let $item = $(evt.target.parentElement);
-  reorderList($item);
+  let $jokeLine = $(evt.target.parentElement);
+  reorderList($jokeLine);
 }
 
+/* Decreases vote count and updates Score Count display */
 function decreaseVote(evt) {
   evt.preventDefault();
   let i = evt.target.parentElement.id
   jokeList[i] -= 1;
   let $scoreCount = $(evt.target.parentElement).find("span")
   $scoreCount.text(`Score Count: ${jokeList[i]}`)
-  let $item = $(evt.target.parentElement);
-  reorderList($item);
+  let $jokeLine = $(evt.target.parentElement);
+  reorderList($jokeLine);
 }
 
-function reorderList($item) {
-  let score = jokeList[$item.attr("id")];
+/* Reorders list after every vote based on score count */
+function reorderList($jokeLine) {
+  let score = jokeList[$jokeLine.attr("id")];
   for (let i = 0; i < 10; i++) {
-    let $prev = $item.prev();
-    let $next = $item.next();
-    if (score > jokeList[$prev.attr("id")]) {
-      $item.insertBefore($prev);
+    let $prevJoke = $jokeLine.prev();
+    let $nextJoke = $jokeLine.next();
+    if (score > jokeList[$prevJoke.attr("id")]) {
+      $jokeLine.insertBefore($prevJoke);
     }
-    else if (score < jokeList[$next.attr("id")]) {
-      $item.insertAfter($next);
+    else if (score < jokeList[$nextJoke.attr("id")]) {
+      $jokeLine.insertAfter($nextJoke);
     }
   }
 }
